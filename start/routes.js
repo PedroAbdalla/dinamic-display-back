9c407f5d225c73fdfb91f8d3f0c693d8b6c1bd97
Pedro Abdala <pedroabdalla@outlook.com>
2020-09-09 00:32:46 -0300

    add forgot password

--- start/routes.js
+++ start/routes.js
@@ -20,7 +20,7 @@ const Route = use('Route')
 Route.post('files', 'FileController.store')
 Route.post('users', 'UserController.store')
 Route.post('sessions', 'SessionController.store')
-
+Route.post('passwords', 'ForgotPasswordController.store')
 
 //GET
 Route.get('files/:id', 'FileController.show')
